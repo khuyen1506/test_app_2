40a7d9ede36cd7c6b5ac6243665c35f62e6ca435
khuyenho <khuyenho93@gmail.com>
2023-02-22 15:55:32 +0700

    init project

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,18 @@
+class StudentController < ApplicationController
+
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+  
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+   
+end
