9fb01be0be2bcfb3c69aff33e959419bdf89dab7
khuyenho <khuyenho93@gmail.com>
2023-02-23 04:23:11 +0700

    Make app production

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,18 +1,9 @@
 class StudentController < ApplicationController
 
-  attr_accessor :first_name, :last_name, :email, :username, :password
-  
-  def initialize(firstname, lastname, username, email, password)
-    @first_name = firstname
-    @last_name = lastname
-    @username = username
-    @email = email
-    @password = password
+  def home
   end
   
-  def to_s
-    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
-                  email address: #{@email}"
+  def about
   end
    
 end
